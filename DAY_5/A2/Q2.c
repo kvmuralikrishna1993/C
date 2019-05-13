619e8645e28904f6df3710390f0fb18572d78374
Murali Krishna Kolapalli <kvmuralikrishna1993@msitprogram.net>
2019-05-13 14:34:48 +0530

    submit2

--- DAY_5/A2/Q2.c
+++ DAY_5/A2/Q2.c
@@ -5,15 +5,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 int check_e(char str[]);
 int getpower(char str[]);
+int getindex_e(char str[]);
+float getvalue(char str[]);
+
 int main(int argc, char const *argv[])
 {
-	char str[] = " -2309.12E-15";
+	char str[] = "-2309.12E-15";
 	float value;
 	if(check_e(str)) {
-
+		int power = getpower(str);
+		float value = getvalue(str);
+		float realvalue = value * pow(10,power);
+		printf("%lf\n", realvalue);
+		//output: -2309120117187500032.000000
 	} else {
 		printf("%e\n", atof(str));
 	}
@@ -31,11 +39,34 @@ int check_e(char str[]) {
 }
 
 int getpower(char str[]) {
-	for (int i = strlen(str)-1; i >=0 ; i--) {
-		if(str[i] == '-') {
-			break;
-		}
-
+	int index = getindex_e(str);
+	int size = strlen(str) - (index+2);
+	char power[size];
+	int j = 0;
+	for (int i = index+2; i < strlen(str) ; i++) {
+		power[j] = str[i];
+		j++;
 	}
-	return 0;
+	return atoi(power);
 }
+
+int getindex_e(char str[]) {
+	for (int i = strlen(str); i >= 0; i--) {
+		if(str[i] == 'E' || str[i] == 'e') {
+			return i;
+		}
+	}
+	return -1;
+}
+
+float getvalue(char str[]){
+	int index = getindex_e(str);
+	char value[index];
+	int j = 0;
+	for (int i = 0; i < index; i++) {
+		value[j] = str[i];
+		j++;
+	}
+	return atof(value);
+}
+
